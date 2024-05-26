881f211c9eabf702aa01ac4760f5df94b8524186
Jayanth <jay.codmik.18@gmail.com>
2024-05-26 19:11:37 +0530

    sadada

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <div className="root-container">
         <div className="wrapper">
 
-          hi fucking 
+          hi fucking all
           {children}
         </div>
       </div>
